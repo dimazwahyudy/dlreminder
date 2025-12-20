237875625813655d3ef5d7e88c6cec3d7d769ff6
Dimaz Wahyudy <dimazwahyudyy@gmail.com>
2025-12-21 00:57:31 +0700

    a

--- config/config.php
+++ config/config.php
@@ -1,13 +1,8 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../'); 
-$dotenv->load();
-
-$host = $getenv['DB_HOST'];
-$user = $getenv['DB_USER'];
-$pass = $getenv['DB_PASS'];
-$db   = $getenv['DB_NAME'];
+$host = "localhost";
+$user = "dlreminderfp";
+$pass = "finalproject321";
+$db   = "FinalProject";
 
 // Use Database class for connection while keeping $conn variable for backward compatibility
 require_once __DIR__ .  '/../app/Controllers/Database.php';
